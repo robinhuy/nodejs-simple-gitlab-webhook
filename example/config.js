41c991082f1b66306240c896de3b2bb801af18c6
Robin Huy <robinhuy@users.noreply.github.com>
2016-11-28 11:47:53 +0700

    Create sample config.js

---
+++ example/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    projects: [
+        {
+            url: "git@gitlab:robinhuy/demo.git",
+            container: "demo",
+            path: "/home/huy/app/demo"
+        }
+    ]
+}
